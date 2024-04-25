d46ae326a6d3d945078775c764c87b9ed7b99016
Danarose105 <103814272+Danarose105@users.noreply.github.com>
2024-04-25 17:07:45 +0300

    התחברות לממשק עם גוגל

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Programmin2_classroom.Client;
+using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -8,5 +9,8 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddSingleton<DateService>(); // Register your custom service here
+builder.Services.AddOidcAuthentication(options => {
+    builder.Configuration.Bind("Local", options.ProviderOptions);
+});
 await builder.Build().RunAsync();
 
